48b8f49de71f171afc2a8fc201d9260e1521d07b
yangyangers <117732234+yangyangers@users.noreply.github.com>
2025-03-07 21:55:20 +0800

    Add Lopez_TicTacToe/wwwroot/js/site.js 1741355719815

---
+++ Lopez_TicTacToe/wwwroot/js/site.js
@@ -0,0 +1,74 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let gameActive = true;
+
+const statusDisplay = document.querySelector('.status');
+const cells = document.querySelectorAll('.cell');
+
+function handleCellClick(clickedCell, clickedCellIndex) {
+    if (board[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    board[clickedCellIndex] = currentPlayer;
+    clickedCell.classList.add(currentPlayer === 'X' ? 'x' : 'o');
+    checkResult();
+}
+
+function checkResult() {
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const [a, b, c] = winningConditions[i];
+        if (board[a] === '' || board[b] === '' || board[c] === '') {
+            continue;
+        }
+        if (board[a] === board[b] && board[a] === board[c]) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = `Player ${currentPlayer} has won!`;
+        gameActive = false;
+        return;
+    }
+
+    if (!board.includes('')) {
+        statusDisplay.innerHTML = 'Game ended in a draw!';
+        gameActive = false;
+        return;
+    }
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function restartGame() {
+    gameActive = true;
+    currentPlayer = 'X';
+    board = ['', '', '', '', '', '', '', '', ''];
+    cells.forEach(cell => {
+        cell.classList.remove('x', 'o');
+    });
+    statusDisplay.innerHTML = '';
+}
+
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => handleCellClick(cell, index));
+});
+
+document.querySelector('.restart').addEventListener('click', restartGame);
